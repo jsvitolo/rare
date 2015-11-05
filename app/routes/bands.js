fa491b3babf2f4b8a9cf8d633246c78d20e64772
Jones Vitolo <jsvitolo@gmail.com>
2015-11-05 09:01:52 -0200

    Advanced routing

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -10,17 +10,17 @@ var blackDog = Song.create({
   rating: 3
 });
 
-var yellowLedbetter = Song.create({
-  title: 'Yellow Ledbetter',
-  band: 'Pearl Jam',
-  rating: 4
-});
-
-var daughter = Song.create({
-  title: 'Daughter',
-  band: 'Pearl Jam',
-  rating: 5
-});
+// var yellowLedbetter = Song.create({
+//   title: 'Yellow Ledbetter',
+//   band: 'Pearl Jam',
+//   rating: 4
+// });
+//
+// var daughter = Song.create({
+//   title: 'Daughter',
+//   band: 'Pearl Jam',
+//   rating: 5
+// });
 
 var pretender = Song.create({
   title: 'The Pretender',
@@ -35,7 +35,13 @@ var BandsCollection = Ember.Object.extend({
 });
 
 var ledZeppelin = Band.create({ name: 'Led Zeppelin', songs: [blackDog] });
-var pearlJam = Band.create({ name: 'Pearl Jam', songs: [daughter, yellowLedbetter] });
+
+var pearlJam = Band.create({
+  name: 'Pearl Jam',
+  description: 'Pearl Jam is an American rock band, formed in Seatle, Washigton in 1990',
+  // songs: [daughter, yellowLedbetter]
+});
+
 var fooFighters = Band.create({ name: 'Foo Fighters', songs: [pretender] });
 
 var bands = BandsCollection.create();
